3033db9bd09b9a270e078edd882520b1240bbdd5
Agustin-Lovera04 <agusslovera04@gmail.com>
2023-12-15 19:45:41 -0300

    Entrega Desafio N°5

---
+++ Entregable N°5/src/router/chat-router.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { auth } from "./viewsRouter.js";
+export const router = Router();
+
+router.get("/", auth,(req, res) => {
+  try {
+    res.status(200).render("chat");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
